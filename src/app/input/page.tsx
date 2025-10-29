43183f94b9bb2bfcddc34ff2e3748618a08961a8
asa129 <asa.jad1290@gmail.com>
2025-10-29 21:54:03 +0900

    shadcn追加

---
+++ src/app/input/page.tsx
@@ -0,0 +1,18 @@
+import { TextareaWithText } from "@/components/ui/textareaWithText";
+
+export default async function page({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const params = await searchParams;
+  return (
+    <div>
+      {/* <Header /> */}
+
+      <TextareaWithText />
+      <p>タイトル:{params.title}</p>
+      <p>URL:{params.url}</p>
+    </div>
+  );
+}
